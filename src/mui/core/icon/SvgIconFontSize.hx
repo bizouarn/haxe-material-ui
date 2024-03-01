cae365f9e769467967cf534fef0bf984e453b268
Aymeric <aymeric.bizouarn@hotmail.fr>
2024-03-01 09:47:56 +0100

    Add missing SvgIconFontSize Medium

--- src/mui/core/icon/SvgIconFontSize.hx
+++ src/mui/core/icon/SvgIconFontSize.hx
@@ -2,6 +2,7 @@ package mui.core.icon;
 
 @:enum abstract SvgIconFontSize(String) {
 	var Default = "default";
+	var Medium = "medium";
 	var Inherit = "inherit";
 	var Small = "small";
 	var Large = "large";
